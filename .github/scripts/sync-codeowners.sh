0d7d98c538f534263f951ece78b496d97d3b3366
Chris Werner Rau <cwr@teuto.net>
2022-09-29 13:44:55 +0200

    feat: initial CI commit

---
+++ .github/scripts/sync-codeowners.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+for DIR in ./charts/*; do
+  FILE="$DIR/Chart.yaml"
+  DIR="${DIR//\./}"
+  MAINTAINERS="$(yq e '.maintainers.[].name' "$FILE" | sed 's/^/@/' | sort --ignore-case | tr '\r\n' ' ')"
+  echo -e "$DIR/ $MAINTAINERS"
+done | sort
+
---
+++ .github/scripts/validate-pullrequest.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+set -exu
+set -o pipefail
+
+: "${PR_TITLE:?Environment variable must be set}"
+
+repo_root=$(git rev-parse --show-toplevel)
+
+changed=$(ct list-changed)
+
+if [[ -z "$changed" ]]; then
+    exit 0
+fi
+
+if [[ "$PR_TITLE" =~ chore(deps)* ]]; then
+  exit 0
+fi
+
+num_changed=$(wc -l <<< "$changed")
+
+if ((num_changed > 1)) && ! [[ "$PR_TITLE" =~ chore(deps):* ]]; then
+    echo "This PR has changes to multiple charts. Please create individual PRs per chart." >&2
+    exit 1
+fi
+
+# Strip charts directory
+changed="${changed##*/}"
+
+if ! cog verify "$PR_TITLE"; then
+  echo "PR title must be a conventional commit message"
+  exit 1
+fi
+
+if [[ "$PR_TITLE" != *"($changed):"* ]]; then
+    echo "PR title must start with '\$type($changed):'." >&2
+    exit 1
+fi
