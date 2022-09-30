c3ad7b00cf044b092369272eb7d8e16ee1adeb64
Chris Werner Rau <cwr@teuto.net>
2022-09-30 12:25:09 +0200

    fix(documentation): only sync codeowners on existing charts

--- .github/scripts/sync-codeowners.sh
+++ .github/scripts/sync-codeowners.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 for DIR in ./charts/*; do
+  [[ "$DIR" = "charts/*" ]] && continue
   FILE="$DIR/Chart.yaml"
   DIR="${DIR//\./}"
   MAINTAINERS="$(yq e '.maintainers.[].name' "$FILE" | sed 's/^/@/' | sort --ignore-case | tr '\r\n' ' ')"
