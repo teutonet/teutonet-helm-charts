688c9c23263c7496bcb80afcd0f432e1ff8ea2fc
Chris Werner Rau <cwr@teuto.net>
2023-07-07 09:57:24 +0200

    feat(ci): automatically label PRs with their chart

--- .github/scripts/validate-and-label-pullrequest.sh
+++ .github/scripts/validate-and-label-pullrequest.sh
@@ -36,3 +36,10 @@ if ! cog verify "$PR_TITLE" 2>&1 | grep -Eq "^\s+Scope: $changed(/.+|)\$"; then
   echo "PR title must have scope '$changed/\$subscope'" >&2
   exit 1
 fi
+
+curl --silent --fail-with-body \
+  -X POST \
+  -H 'Accept: application/vnd.github+json' \
+  -H "Authorization: token ${GITHUB_TOKEN}" \
+  "${GITHUB_API_URL}/repos/${GITHUB_REPOSITORY}/issues/${ISSUE_NUMBER}/labels" \
+  -d '{"labels":["'"$changed"'"]}'
