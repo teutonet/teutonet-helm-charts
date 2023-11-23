0bc534b214c7d8629f01a29aec6aefdd3c21e325
Chris Werner Rau <cwr@teuto.net>
2023-11-23 11:17:44 +0100

    feat(ci): validate and label pull requests separately

---
+++ .github/scripts/validate-pullrequest.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+[[ "$RUNNER_DEBUG" == 1 ]] && set -x
+
+set -eu
+set -o pipefail
+
+: "${PR_TITLE:?Environment variable must be set}"
+
+changed="${CHANGED_CHART?Environment variable must be set}"
+
+if ! cog verify "$PR_TITLE"; then
+	echo "PR title must be a conventional commit message" >&2
+	exit 1
+fi
+
+if [[ -n "$changed" ]] && ! cog verify "$PR_TITLE" 2>&1 | grep -Eq "^\s+Scope: $changed(/.+|)\$"; then
+	echo "PR title must have scope '$changed/\$subscope'" >&2
+	exit 1
+fi
