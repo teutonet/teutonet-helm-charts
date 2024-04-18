358b9e26bdcd525debac397f5c3aafa0a0c274ba
Chris Werner Rau <cwr@teuto.net>
2024-04-18 16:26:05 +0200

    feat(base-cluster/flux): add option to add a path inside the kustomiz… (#876)

--- charts/base-cluster/ci/flux-gitrepositories-gen-values.sh
+++ charts/base-cluster/ci/flux-gitrepositories-gen-values.sh
@@ -7,12 +7,14 @@ set -o pipefail
 
 i=0
 for url in 'url="https://github.com"' 'url="ssh://git@github.com/group/name.git"'; do
-  for extra in 'branch="main"' 'commit="549aee1"' 'semver="1.0.0"' 'tag="2.0.1"' ''; do
-    for decr in 'decryption={provider:"sops"}' ''; do
-      for auth in 'username="user" | .password="pass"' ''; do
-        for git in 'gitInterval="5m"' ''; do
-          [[ $url = *ssh* ]] && [[ -n "$auth" ]] && continue
-          yq -y -n "{main$((i++)): ({} | .$url | .$extra | .$decr | .$auth | .$git)}"
+  for path in 'path="prod"' ''; do
+    for extra in 'branch="main"' 'commit="549aee1"' 'semver="1.0.0"' 'tag="2.0.1"' ''; do
+      for decr in 'decryption={provider:"sops"}' ''; do
+        for auth in 'username="user" | .password="pass"' ''; do
+          for git in 'gitInterval="5m"' ''; do
+            [[ $url = *ssh* ]] && [[ -n "$auth" ]] && continue
+            yq -y -n "{main$((i++)): ({} | .$url | .$path | .$extra | .$decr | .$auth | .$git)}"
+          done
         done
       done
     done
