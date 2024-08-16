dcb50085bd998c933d4ea06cf61daa1165a1e715
Chris Werner Rau <cwr@teuto.net>
2024-08-16 15:40:51 +0200

    feat(ci): add cve scanning

---
+++ .github/scripts/generate-sarif-reports.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+[[ "$RUNNER_DEBUG" == 1 ]] && set -x
+[[ -o xtrace ]] && export RUNNER_DEBUG=1
+
+set -eu
+set -o pipefail
+
+function createSarifReports() {
+  local chart="${1?}"
+  mkdir -p reports
+
+  yq -r '.annotations["artifacthub.io/images"]' "$chart/Chart.yaml" |
+    yq -r '.[] | .image' |
+    parallel -P 0 -k generateSarifReport "$chart" "{}" "reports/{#}.sarif.json"
+}
+
+function generateSarifReport() {
+  local chart="${1?}"
+  local image="${2?}"
+  local outFile="${3?}"
+  local locationsJson
+  # shellcheck disable=SC2016
+  locationsJson="$(yq --arg image "$image" -r '.annotations["artifacthub.io/images"] | split("\n")[] | select(contains($image))' "$chart/Chart.yaml" |
+    awk '{print $NF}' |
+    jq -r -c -Rn '[inputs] | map({fullyQualifiedName: .})')"
+  trivy image "$image" -f sarif --quiet --ignore-unfixed | jq -r --argjson locations "$locationsJson" '.runs |= map(.results |= map(.locations |= ([$locations[], .[]])))' >"$outFile"
+}
+export -f generateSarifReport
+
+if [[ "$#" == 1 ]] && [[ -d "$1" ]]; then
+  generateSarifReports "$1"
+else
+  result=0
+  for chart in charts/*; do
+    [[ "$chart" == "charts/*" ]] && continue
+
+    if ! generateSarifReports "$chart"; then
+      result=1
+    fi
+  done
+  exit "$result"
+fi
