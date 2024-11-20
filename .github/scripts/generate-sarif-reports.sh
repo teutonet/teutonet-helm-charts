f2688ded26f182e89aa377c5520791bf92a3d88d
Chris Werner Rau <cwr@teuto.net>
2024-11-20 13:46:19 +0100

    fix(ci): now for real (#1245)
    slipped through review...

--- .github/scripts/generate-sarif-reports.sh
+++ .github/scripts/generate-sarif-reports.sh
@@ -7,7 +7,7 @@ set -eu
 set -o pipefail
 
 declare -A IMAGE_PULL_TOKENS=(
-  [registry - gitlab.teuto.net]="${TEUTO_PORTAL_WORKER_PULL_TOKEN}"
+  ["registry-gitlab.teuto.net"]="${TEUTO_PORTAL_WORKER_PULL_TOKEN}"
 )
 
 function createSarifReports() {
