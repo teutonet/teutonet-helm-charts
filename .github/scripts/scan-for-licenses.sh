f2a29c7799eda02e3f5493df55563660a546712a
marvinWolff <36923121+marvinWolff@users.noreply.github.com>
2024-11-26 10:40:14 +0100

    feat(ci): revert last two commits (#1257)

--- .github/scripts/scan-for-licenses.sh
+++ .github/scripts/scan-for-licenses.sh
@@ -8,50 +8,19 @@ set -o pipefail
 
 WHITELIST=(
   "AGPL-3.0" # We're not writing software 🤷
-  "AGPL-3.0-only"
-  "AGPL-3.0-or-later"
   "CC-BY-SA-3.0"
-  "CDDL-1.0"
-  "CDDL-1.1"
-  "EPL-1.0"
-  "EPL-2.0"
   "CPL-1.0"
-  "GPLv2"
   "GPL-1.0"
-  "GPL-1.0-only"
-  "GPL-1.0-or-later"
-  "GPLv2"
-  "GPLv2+"
-  "GPLv2 with exceptions"
-  "GPLv3+"
   "GPL-2.0"
-  "GPL-2.0-only"
-  "GPL-2.0-or-later"
   "GPL-2.0-with-autoconf-exception"
-  "GPL-2.0-with-autoconf-exception+"
   "GPL-2.0-with-bison-exception"
-  "GPL-2.0-with-bison-exception+"
-  "GPL-2.0-with-classpath-exception"
   "GPL-3.0"
-  "GPL-3.0-only"
-  "GPL-3.0-or-later"
-  "GPL-3.0-with-autoconf-exception+"
   "GPL-3.0-with-autoconf-exception"
-  "LGPLv2"
-  "LGPLv2+"
   "LGPL-2.0"
-  "LGPL-2.0-only"
-  "LGPL-2.0-or-later"
   "LGPL-2.1"
-  "LGPL-2.1-only"
-  "LGPL-2.1-or-later"
-  "LGPLv3+"
   "LGPL-3.0"
-  "LGPL-3.0-only"
-  "LGPL-3.0-or-later"
   "MPL-1.1"
   "MPL-2.0"
-  "MPLv2.0"
   "Ruby"
   "Sleepycat"
   "WTFPL"
@@ -65,7 +34,6 @@ function scanLicenses() {
   local unacceptedLicenses=()
   local unacceptedLicense
   licenseMap="$(yq -r '.annotations["artifacthub.io/images"]' "$chart/Chart.yaml" | yq -r '.[] | .image' |
-    sed -e '/aquasecurity\/trivy-operator/d' -e '/teuto-portal-k8s-worker/d' |
     parallel -k trivy image {} --severity HIGH,CRITICAL,MEDIUM -f json --scanners license --license-full --quiet |
     jq -s -r "$licenseConversionJq")"
   mapfile -t unacceptedLicenses < <(jq <<<"$licenseMap" -r --argjson acceptedLicenses "[\"$(echo -n "${WHITELIST[@]}" | tr " " \\n |
